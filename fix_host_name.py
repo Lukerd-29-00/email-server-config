d5de8a68d04b69ea0ae92b997cd05d47a81c8b28
= <=>
2020-07-13 09:58:36 -0400

    fixed and commented python3 script to work without main.cf present. Added explanation to README.

--- fix_host_name.py
+++ fix_host_name.py
@@ -1,16 +1,13 @@
+#read the hostname, without the trailing newline. Requires the hostname to be written into a file named .host within the CWD/PWD. 
 f = open(".host",'r')
 name = f.read().strip()
 f.close()
-f = open('main.cf','r')
-txt = f.read()
-f.close()
-f = open('main.cf','w')
-txt = txt.replace('brass',name)
-f.write(txt)
+#read the dovecot.conf configuration file from the current working directory.
 f.close()
 f = open('dovecot.conf','r')
 txt = f.read()
 f.close()
+#modify the file by replacing the word 'studio' with the hostname of the current computer.
 f = open('dovecot.conf','w')
 txt = txt.replace('studio',name)
 f.write(txt)
